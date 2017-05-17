c40d22eac04b6e54216ae493fb77d861fd3edd20
jnhustin <jnhustin@gmail.com>
2017-05-17 10:00:18 -0700

    completed linked list implementation

--- linked-list-5-16-17.js
+++ linked-list-5-16-17.js
@@ -10,57 +10,78 @@ class LinkList {
     this.start  = null;
     this.end    = null;
   }
-}
-LinkList.prototype.addAtStart = function(data) {
-  let n = new Node(data);
+  addAtStart(data) {
+    let n = new Node(data);
 
-  if (this.start == null) {
-    this.start  = n;
-    this.end    = n;
-  } 
-  else if (this.start.next == null) {
-    n.next      = this.start;
-    this.start  = n;
-  } 
-  else {
-    n.next = this.start;
-    this.start = n;
-    this.start.next;
+    if (this.start == null) {
+      this.start  = n;
+      this.end    = n;
+    } 
+    else if (this.start.next == null) {
+      n.next      = this.start;
+      this.start  = n;
+    } 
+    else {
+      n.next = this.start;
+      this.start = n;
+      this.start.next;
+    } 
   }
+  addAtEnd(data) {
+    let n = new Node(data);
 
-}
-LinkList.prototype.addAtEnd = function(data) {
-  let n = new Node(data);
-
-}
-LinkList.prototype.removeAtStart = function() {
-
-}
-LinkList.prototype.removeAtEnd = function() {
-  let temp = this.start;
-
-  if (this.start === null) {
-    return;
+    this.end.next = n;
+    this.end = n;
   }
-  else if (this.start.next === null) {
-    this.start  = null;
-    this.end    = null;
+  removeAtEnd() {
+    let temp = this.start;
+
+    if (this.start === null) {
+      return;
+    }
+    else if (this.start.next === null) {
+      this.start  = null;
+      this.end    = null;
+    }
+    while (temp.next !== this.end) {
+      temp = temp.next;
+    }
+    this.end    = temp;
+    temp.next   = null;
   }
-  while (temp.next !== this.end) {
-    temp = temp.next;
+  removeAt(node) {
+
   }
-  this.end    = temp;
-  temp.next   = null;
-}
-LinkList.prototype.removeAt = function(node) {
+  printData() {
+    if (!this.start) {
+      return 'empty';
+    }
 
-}
-LinkList.prototype.printData = function() {
+    let arr = [];
+    let current = this.start;
+    
+    arr.push(current.data);
+    while (current.next !== null) {
+      arr.push(current.next.data);
+      current = current.next;
+    }
 
-}
-LinkList.prototype.printLength = function() {
+    return arr;
+  }
+  printLength() {
+    let ctr = 1;
+    let current = this.start;
+    
+    while (current.next != null) {
+      current = current.next;
+      ctr++;
+    }
 
+    return ctr;
+  }
 }
+
+
 var one = new LinkList;
 one.addAtStart(3);
 console.log(one);
@@ -69,3 +90,7 @@ one.addAtStart(1);
 console.log(one);
 one.removeAtEnd();
 console.log(one);
+one.addAtEnd(3);
+console.log(one);
+console.log(one.printData());
+console.log(one.printLength());
