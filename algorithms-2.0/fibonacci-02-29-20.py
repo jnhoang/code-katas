ac831afa708620ebd77f19a8c65320a9422b436e
jnhustin <jnhustin@gmail.com>
2020-02-29 13:12:29 -0800

    added cache

--- algorithms-2.0/fibonacci-02-29-20.py
+++ algorithms-2.0/fibonacci-02-29-20.py
@@ -8,24 +8,34 @@
     3. iteratively
 """
 
-def fibonacci(index):
+def fibonacci(index, cache={}):
 
-  # naive solution
+  # # naive solution
+  # if index == 0:
+  #   print('no thanks')
+  # elif index == 1 or index == 2:
+  #   return 1
+  # else:
+  #   return fibonacci(index - 1) + fibonacci(index - 2)
+
+  # with cache
   if index == 0:
     print('no thanks')
   elif index == 1 or index == 2:
     return 1
+  elif cache.get(index):
+    return cache[index]
   else:
-    return fibonacci(index - 1) + fibonacci(index - 2)
-
+    cache[index] = fibonacci(index - 1, cache) + fibonacci(index - 2, cache)
+    return cache[index]
 
 
 
 if __name__ == '__main__':
   expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
  987, 1597, 2584, 4181, 6765]
-  result = [ fibonacci(i) for i in range(1, 20 + 1) ]
+  # result = [ fibonacci(i) for i in range(1, 20 + 1) ]
   # uncomment this one after memoized solution is done
-  # result = [ fibonacci(i) for i in range(1, 100) ]
+  result = [ fibonacci(i) for i in range(1, 100) ]
   print('results     : ', result)
-  print('20 expected : ', expected)
+  # print('20 expected : ', expected)
