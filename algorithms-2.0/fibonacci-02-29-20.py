385b654a8a6cb11a189cbf82f1d56ea4d71b51e0
jnhustin <jnhustin@gmail.com>
2020-02-29 19:26:44 -0800

    add iterative fibonacci

--- algorithms-2.0/fibonacci-02-29-20.py
+++ algorithms-2.0/fibonacci-02-29-20.py
@@ -8,17 +8,17 @@
     3. iteratively
 """
 
+# recursive naive solution
+def fibonacci(index):
+  if index == 0:
+    print('no thanks')
+  elif index == 1 or index == 2:
+    return 1
+  else:
+    return fibonacci(index - 1) + fibonacci(index - 2)
+
+# with cache
 def fibonacci(index, cache={}):
-
-  # # naive solution
-  # if index == 0:
-  #   print('no thanks')
-  # elif index == 1 or index == 2:
-  #   return 1
-  # else:
-  #   return fibonacci(index - 1) + fibonacci(index - 2)
-
-  # with cache
   if index == 0:
     print('no thanks')
   elif index == 1 or index == 2:
@@ -30,11 +30,21 @@ def fibonacci(index, cache={}):
     return cache[index]
 
 
+# iterative solution
+def fibonacci(index):
+  sequence =  []
+
+  for i in range (index):
+    if i == 0 or i == 1:
+      sequence.append(1)
+    else:
+      sequence.append( sequence[i - 1] + (sequence[i - 2]))
+  return sequence[index - 1]
 
 if __name__ == '__main__':
   expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
  987, 1597, 2584, 4181, 6765]
-  # result = [ fibonacci(i) for i in range(1, 20 + 1) ]
+  # result = [ fibonacci(i) for i in range(1, 5 + 1) ]
   # uncomment this one after memoized solution is done
   result = [ fibonacci(i) for i in range(1, 100) ]
   print('results     : ', result)
